4be7715e69f12cfb526c7a3fe706a58f05173771
Mark jeff van Haastert <mark.h@bit-students.com>
2017-12-05 14:37:48 +0100

    Opdracht 2

--- mijnjavascript.js
+++ mijnjavascript.js
@@ -1,8 +1,4 @@
-var mijnauto = {
-    merk: "Ford",
-    type: "Mondeo",
-    aantalWielen: 4,
-    kleur: "blauw"
-}
-
-console.log(mijnauto.kleur)
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("aap1").src = "img/aap1.jpg";
+document.getElementById("aap2").src = "img/aap2.jpg";
