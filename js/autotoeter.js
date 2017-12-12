71983bffed33989f426d8e7d50b7aef4e3fa9e65
Mark jeff van Haastert <mark.h@bit-students.com>
2017-12-12 10:37:19 +0100

    Javascript Functies

---
+++ js/autotoeter.js
@@ -0,0 +1,29 @@
+var mijnauto = {
+    kleur: "blauw",
+    merk: "Ford",
+    snelheid: 0,
+
+    gasgeven: function(){
+        this.snelheid += 5
+        console.log(this.snelheid);
+    },
+
+    toeteren: function () {
+        console.log("toet!")
+
+    }
+}
+
+console.log(mijnauto.kleur)
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.toeteren();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.toeteren();
--- mijnjavascript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
-document.getElementById("subkop").innerText = "Mijn geweldige subkop";
-document.getElementById("aap1").src = "img/aap1.jpg";
-document.getElementById("aap2").src = "img/aap2.jpg";
